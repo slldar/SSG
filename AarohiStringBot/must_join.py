7fe556fa3d65a2781cbec1433ceab7710f7bc123
slldar <165353541+slldar@users.noreply.github.com>
2024-04-15 22:23:49 +0200

    Update must_join.py

--- AarohiStringBot/must_join.py
+++ AarohiStringBot/must_join.py
@@ -20,15 +20,16 @@ async def must_join_channel(bot: Client, msg: Message):
                 link = chat_info.invite_link
             try:
                 await msg.reply_photo(
-                    photo="https://te.legra.ph/file/17673c2c6b0ff648e5983.png", caption=f"» ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ [sᴜᴘᴘᴏʀᴛ]({link}) ʏᴇᴛ, ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴜsᴇ ᴍᴇ ᴛʜᴇɴ ᴊᴏɪɴ [sᴜᴘᴘᴏʀᴛ]({link}) ᴀɴᴅ sᴛᴀʀᴛ ᴍᴇ ᴀɢᴀɪɴ !",
+                    photo="https://graph.org/file/bcc6c4434ca6b138924e0.jpg", caption=f"- صلي علي نبينا محمد .
+- قناه الدعم اشترك فيها ثم ارسل /start [الدعم]({link}) .",
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("</> ᴍᴜsɪᴄ ʙᴏᴛ[❣️]", url=f"https://t.me/Alone_Dil_bot?startgroup=true"),
+                                InlineKeyboardButton(". المطور .", url=f"https://t.me/Q_z_T"),
                             ],
                             [
-                                InlineKeyboardButton("</> sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ[❣️]", url=link),
-                                InlineKeyboardButton("</> ᴍʏ ʟɪғᴇʟɪɴᴇ[❣️]", url=f"https://t.me/aarohi_ki_duniya"),
+                                InlineKeyboardButton(". قناه الدعم .", url=link),
+                                InlineKeyboardButton(". قناه كتابات .", url=f"https://t.me/c8_8x"),
                             ],
                         ]
                     )
